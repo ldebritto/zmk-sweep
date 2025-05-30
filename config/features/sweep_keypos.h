d7593881c647bd9bb09ca42693369c0269670cd9
ldebritto <ldebritto@gmail.com>
2025-05-30 12:04:11 -0300

    Experimenting with sweep_keypos.h to make .dtsi files interchangeable between keyboards

---
+++ config/features/sweep_keypos.h
@@ -0,0 +1,63 @@
+// Sweep key positions cheat sheet
+// 
+// 0    1    2    3    4        5    6    7    8    9
+// 10   11   12   13   14       15   16   17   18   19
+// 20   21   22   23   24       25   26   27   28   29
+//                30   31       32   33
+//s
+// These will translate to
+// 
+// LT0  LT1  LT2  LT3  LT4      RT0  RT1  RT2  RT3  RT4
+// LM0  LM1  LM2  LM3  LM4      RM0  RM1  RM2  RM3  RM4
+// LB0  LB1  LB2  LB3  LB4      RB0  RB1  RB2  RB3  RB4
+//                LIT  LHT      RHT  RIT
+
+#pragma once
+
+// Left top keys
+#define LT0 0
+#define LT1 1
+#define LT2 2
+#define LT3 3
+#define LT4 4
+
+// Right top keys
+#define RT0 5
+#define RT1 6
+#define RT2 7
+#define RT3 8
+#define RT4 9
+
+// Left middle keys
+#define LM0 10
+#define LM1 11
+#define LM2 12
+#define LM3 13
+#define LM4 14
+
+// Right middle keys
+#define RM0 15
+#define RM1 16
+#define RM2 17
+#define RM3 18
+#define RM4 19
+
+// Left bottom keys
+#define LB0 20
+#define LB1 21
+#define LB2 22
+#define LB3 23
+#define LB4 24
+
+// Right bottom keys
+#define RB0 25
+#define RB1 26
+#define RB2 27
+#define RB3 28
+#define RB4 29
+
+// Thumbs
+#define LIT 30
+#define LHT 31
+#define RHT 32
+#define RIT 33
