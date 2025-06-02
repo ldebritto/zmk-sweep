526aae60053961afda2fdc2d7c86a8bc8079150b
ldebritto <ldebritto@gmail.com>
2025-06-02 17:07:05 -0300

    timings.h

---
+++ config/features/timings.h
@@ -0,0 +1,14 @@
+#pragma once // This makes so that variables in this file will have precedence over the ones in other files
+
+// Hold-taps
+    #define HOLD_TAP_TERM 280
+    #define QUICK_TAP 200
+    #define REQUIRE_PRIOR_IDLE 200
+    
+// Tap Dance
+    #define TAP_DANCE_TERM 300
+
+// Combos
+    #define SHORT_COMBO_TIMEOUT 60  // Default timeout for combos in ms
+    #define LONG_COMBO_TIMEOUT 100  // Long timeout for four-finger combos in ms
+    #define REQUIRE_PRIOR_IDLE 200
